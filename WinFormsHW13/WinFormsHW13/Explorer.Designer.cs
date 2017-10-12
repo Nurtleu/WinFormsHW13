c718fecb7c9069976fa80325ba9b60af871ade8a
ОмиржанН <@208-12-PC>
2017-10-12 19:10:04 +0600

    HomeWork_WinForms

---
+++ WinFormsHW13/WinFormsHW13/Explorer.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WinFormsHW13
+{
+    partial class Explorer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeViewExplorerFile = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeViewExplorerFile
+            // 
+            this.treeViewExplorerFile.Location = new System.Drawing.Point(13, 13);
+            this.treeViewExplorerFile.Name = "treeViewExplorerFile";
+            this.treeViewExplorerFile.Size = new System.Drawing.Size(367, 379);
+            this.treeViewExplorerFile.TabIndex = 0;
+            // 
+            // Explorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 407);
+            this.Controls.Add(this.treeViewExplorerFile);
+            this.Name = "Explorer";
+            this.Text = "Explorer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewExplorerFile;
+    }
+}
+
---
+++ WinFormsHW13/WinFormsHW13/Explorer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsHW13
+{
+    public partial class Explorer : Form
+    {
+        TreeNode treeNodeDrives;
+        public Explorer()
+        {
+            InitializeComponent();
+            InfoDriveMove();
+        }
+
+        private void TreeDrive(TreeNode driveNodes, string path)
+        {
+            string[] directorios = Directory.GetDirectories(path);
+            foreach (string directory in directorios)
+            {
+                TreeNode treeNodeDrive = new TreeNode();
+                treeNodeDrive.Text = directory.Remove(0, directory.LastIndexOf("\\") + 1);
+                driveNodes.Nodes.Add(treeNodeDrive);
+            }
+        }
+
+        private void InfoDriveMove()
+        {
+            treeNodeDrives = new TreeNode();
+            foreach (DriveInfo infoDrive in DriveInfo.GetDrives())
+            {                
+                TreeDrive(treeNodeDrives, infoDrive.Name);
+                treeViewExplorerFile.Nodes.Add(treeNodeDrives);
+                treeNodeDrives = new TreeNode()
+                {
+                    Text = infoDrive.Name
+                };
+               
+            }
+        }
+    }
+}
